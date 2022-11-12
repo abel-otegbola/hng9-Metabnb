3d84bb1777c3fbff5fd73c9ddfbf004e6dc7b9b7
abel-otegbola <abel15655@gmail.com>
2022-11-12 15:17:59 +0100

    feat: add navbar and hero-section texts

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,9 +1,24 @@
 import "./navbar.css";
+import logo from "../../imgs/logo.png";
+import { FaBars } from "react-icons/fa"
+import { useState } from "react";
 
 const Navbar = () => {
+    const [menuToggle, setMenuToggle] = useState(false)
+
     return (
         <div className="navbar">
-            welcome
+            <img src={logo} height="30px" alt="logo" />
+            <nav className={(menuToggle) ? "active" : ""}>
+                <div className="nav-menu">
+                    <a href="/">Home</a>
+                    <a href="/">Place to stay</a>
+                    <a href="/">NFTs</a>
+                    <a href="/">Community</a>
+                </div>
+                <a className="cto-button" href="/">Connect wallet</a>
+            </nav>
+            <button className="nav-toggler" onClick={() => setMenuToggle(!menuToggle)}><FaBars /></button>
         </div>
     )
 }
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,7 +3,16 @@ import "./home.css";
 const Home = () => {
     return (
         <div className="home">
-
+            <div className="hero-section">
+                <div className="hero-section--texts">
+                    <h1>Rent a <span>Place</span> away from <span>Home</span> in the <span>Metaverse</span></h1>
+                    <p>we provide you access to luxury and affordable houses in the metaverse, get a chance to turn your imagination to reality at your comfort zone</p>
+                    <div className="search-field">
+                        <input type="text" placeholder="Search for location" />
+                        <button>Search</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
