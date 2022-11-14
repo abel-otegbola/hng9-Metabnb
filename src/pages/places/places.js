8885e5667dcab767b0b1d8eab74854cc24ddb62b
abel-otegbola <abel15655@gmail.com>
2022-11-14 14:51:08 +0100

    feat: add location filter toggle functionality

--- src/pages/places/places.js
+++ src/pages/places/places.js
@@ -1,16 +1,32 @@
 import PlaceContainer from "../../components/placeContainer/placeContainer";
 import "./places.css";
 import filterIcon from "../../imgs/filter.svg"
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { PlacesContext } from "../../context/placesContext";
 
 const Places = () => {
     const places = useContext(PlacesContext)
+    const [isOpen, setOpen] = useState(false)
 
     return (
         <div className="places--page">
             <div className="categories">
-                <button>Location <img src={filterIcon} alt="filter icon" width="20px" /></button>
+                <ul className={(isOpen) ? "active" : ""}>
+                    <li>Restuarant</li>
+                    <li>Cottage</li>
+                    <li>Castle</li>
+                    <li>Fantast city</li>
+                    <li>Beach</li>
+                    <li>Cabins</li>
+                    <li>Off-grid</li>
+                    <li>Farm</li>
+                </ul>
+                <button
+                    onClick={() => setOpen(!isOpen)}
+                >
+                    Location 
+                    <img src={filterIcon} alt="filter icon" width="20px" />
+                </button>
             </div>
             <div className="places--list">
                 <div className="places">
